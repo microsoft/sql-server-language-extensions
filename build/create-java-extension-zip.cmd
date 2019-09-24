c07b6a61064f6db95b799b1b34e63223016c0c94
Aniruddh Munde <anmunde@microsoft.com>
2019-09-23 22:24:59 -0700

    fix enlistment root

--- build/create-java-extension-zip.cmd
+++ build/create-java-extension-zip.cmd
@@ -1,12 +1,12 @@
 @ECHO off
 SETLOCAL
 
-SET EnlRoot=%~dp0
+SET EnlRoot=%~dp0..\
 
 SET MSVC_BUILD_CONFIGURATION=debug
-SET BUILD_OUTPUT=%EnlRoot%language-extensions\java\build\windows\x64\%MSVC_BUILD_CONFIGURATION%
+SET BUILD_OUTPUT=%EnlRoot%..\language-extensions\java\build\windows\x64\%MSVC_BUILD_CONFIGURATION%
 
-SET TARGET="%~dp0.build\java-extension\target\%MSVC_BUILD_CONFIGURATION%"
+SET TARGET="%~dp0..\.build\java-extension\target\%MSVC_BUILD_CONFIGURATION%"
 SET OUTPUT_JAR=%TARGET%\mssql-java-lang-extension.jar
 
 mkdir %BUILD_OUTPUT%\packages
@@ -15,7 +15,7 @@ powershell -NoProfile -ExecutionPolicy Unrestricted -Command "Compress-Archive -
 SET MSVC_BUILD_CONFIGURATION=release
 SET BUILD_OUTPUT=%EnlRoot%language-extensions\java\build\windows\x64\%MSVC_BUILD_CONFIGURATION%
 
-SET TARGET="%~dp0.build\java-extension\target\%MSVC_BUILD_CONFIGURATION%"
+SET TARGET="%~dp0..\.build\java-extension\target\%MSVC_BUILD_CONFIGURATION%"
 SET OUTPUT_JAR=%TARGET%\mssql-java-lang-extension.jar
 
 mkdir %BUILD_OUTPUT%\packages
