cf8578b8dd8f02172b304135df3c2859b350f8e7
Jonathan Zhu <joz@microsoft.com>
2019-11-12 17:04:30 +0000

    Merged PR 2245575: Remove all rights reserved from header in Linux for legal
    Missed the Linux files when doing previous cleanup because they are not in the VS solution

--- language-extensions/java/src/linux/JavaExtensionUtils_linux.cpp
+++ language-extensions/java/src/linux/JavaExtensionUtils_linux.cpp
@@ -1,5 +1,5 @@
 //*********************************************************************
-// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 //
 // @File: JavaExtensionUtils_linux.cpp
