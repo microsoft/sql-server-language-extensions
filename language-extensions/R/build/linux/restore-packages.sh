ac58b7859aeb1c85443b27386db00295703c7680
Aniruddh Munde <anmunde@microsoft.com>
2020-03-18 23:56:32 +0000

    Merged PR 2702511: Fix RInside package version and restore-packages to check for errors
    Builds from master started failing because RInside version 0.2.15 was no longer valid. The RInside authors increased it to 0.2.16 on 2020/03/14 in R's central repository [https://cran.r-project.org/bin/windows/contrib/3.5/](https://cran.r-project.org/bin/windows/contrib/3.5/
    ). This change is to accommodate the version increase.
    
    This also fixes the restore packages script to check for errors so we don't go ahead in the pipeline if some restore step fails.
    
    PR URL: https://msazure.visualstudio.com/DefaultCollection/One/_git/Data-SQL-Language-Extensions/pullrequest/2702511

--- language-extensions/R/build/linux/restore-packages.sh
+++ language-extensions/R/build/linux/restore-packages.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+function check_exit_code {
+	EXIT_CODE=$?
+	if [ ${EXIT_CODE} -eq 0 ]; then
+		echo $1
+	else
+		echo $2
+		exit ${EXIT_CODE}
+	fi
+}
+
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get --no-install-recommends -y install curl zip unzip apt-transport-https
@@ -17,16 +27,21 @@ MS_ROPEN_VERSION="3.5.2"
 MS_ROPEN_MRO="${MS_ROPEN}-mro-${MS_ROPEN_VERSION}"
 
 apt-get -q -y install "${MS_ROPEN_MRO}"
+check_exit_code "Success: Installed R runtime." "Error: Failed to install R runtime."
 
 R_HOME=/opt/microsoft/ropen/3.5.2/lib64/R
 R_LIBRARY_PATH=${R_HOME}/library
 
 # Install Rcpp.
 #
-/opt/microsoft/ropen/3.5.2/lib64/R/bin/R -e "install.packages('https://cran.r-project.org/src/contrib/Rcpp_1.0.3.tar.gz', lib = '${R_LIBRARY_PATH}')"
+${R_HOME}/bin/R -e "install.packages('https://cran.r-project.org/src/contrib/Rcpp_1.0.3.tar.gz', lib = '${R_LIBRARY_PATH}')"
+${R_HOME}/bin/R -e "stopifnot(require(Rcpp))"
+check_exit_code "Success: Installed Rcpp package." "Error: Failed to install Rcpp package."
 
 # Install RInside.
 #
-/opt/microsoft/ropen/3.5.2/lib64/R/bin/R -e "install.packages('https://cran.r-project.org/src/contrib/RInside_0.2.15.tar.gz', lib = '${R_LIBRARY_PATH}')"
+${R_HOME}/bin/R -e "install.packages('https://cran.r-project.org/src/contrib/RInside_0.2.16.tar.gz', lib = '${R_LIBRARY_PATH}')"
+${R_HOME}/bin/R -e "stopifnot(require(RInside))"
+check_exit_code "Success: Installed RInside package." "Error: Failed to install RInside package."
 
 exit $?
--- language-extensions/R/build/windows/restore-packages.cmd
+++ language-extensions/R/build/windows/restore-packages.cmd
@@ -6,10 +6,12 @@ SET ENL_ROOT=%~dp0..\..\..\..
 REM Call the root level restore-packages
 REM
 CALL %ENL_ROOT%\restore-packages.cmd
+CALL :CHECKERROR %ERRORLEVEL% "Error: Failed to restore common nuget packages." || EXIT /b %ERRORLEVEL%
 
 REM Get the MRO nuget package
 REM
 nuget restore %ENL_ROOT%\language-extensions\R\packages.config -PackagesDirectory %ENL_ROOT%\packages
+CALL :CHECKERROR %ERRORLEVEL% "Error: Failed to restore nuget packages required for RExtension." || EXIT /b %ERRORLEVEL%
 
 REM Install Rcpp and RInside
 REM
@@ -18,8 +20,14 @@ SET R_BIN_PATH=%R_HOME%\bin
 
 SETLOCAL enabledelayedexpansion
 SET R_LIBRARY_PATH=%R_HOME%\library
-%R_BIN_PATH%\R -e "install.packages('https://cran.r-project.org/bin/windows/contrib/3.5/Rcpp_1.0.3.zip', lib = '!R_LIBRARY_PATH:\=/!')"
-%R_BIN_PATH%\R -e "install.packages('https://cran.r-project.org/bin/windows/contrib/3.5/RInside_0.2.15.zip', lib = '!R_LIBRARY_PATH:\=/!')"
+
+%R_BIN_PATH%\R -e "install.packages('https://cran.r-project.org/bin/windows/contrib/3.5/Rcpp_1.0.3.zip', lib = '!R_LIBRARY_PATH:\=/!', repos = NULL)"
+%R_BIN_PATH%\R -e "stopifnot(require(Rcpp))"
+CALL :CHECKERROR %ERRORLEVEL% "Error: Failed to install Rcpp package" || EXIT /b %ERRORLEVEL%
+
+%R_BIN_PATH%\R -e "install.packages('https://cran.r-project.org/bin/windows/contrib/3.5/RInside_0.2.16.zip', lib = '!R_LIBRARY_PATH:\=/!', repos = NULL)"
+%R_BIN_PATH%\R -e "stopifnot(require(RInside))"
+CALL :CHECKERROR %ERRORLEVEL% "Error: Failed to install RInside package" || EXIT /b %ERRORLEVEL%
 SETLOCAL disabledelayedexpansion
 
 REM Get RTools for mingw32
@@ -27,9 +35,19 @@ REM
 SET RTOOLS_HOME=%ENL_ROOT%\packages\Rtools
 MKDIR %RTOOLS_HOME%
 powershell -Command "Invoke-WebRequest https://cran.r-project.org/bin/windows/Rtools/Rtools35.exe -OutFile %RTOOLS_HOME%\Rtools35.exe"
+CALL :CHECKERROR %ERRORLEVEL% "Error: Failed to download Rtools." || EXIT /b %ERRORLEVEL%
 
 REM Install RTools
 REM
 %RTOOLS_HOME%\Rtools35.exe /VERYSILENT /DIR="C:\Rtools\"
+CALL :CHECKERROR %ERRORLEVEL% "Error: Failed to install Rtools." || EXIT /b %ERRORLEVEL%
 
 EXIT /b %ERRORLEVEL%
+
+:CHECKERROR
+	IF %1 NEQ 0 (
+		ECHO %2
+		EXIT /b %1
+	)
+
+	EXIT /b 0
