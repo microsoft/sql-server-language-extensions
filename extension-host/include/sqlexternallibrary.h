ce1629d03849de05cbc7803758bb7f7b1ba4b2fc
Jonathan Zhu <joz@microsoft.com>
2020-03-27 10:48:45 -0700

    Add the extension-host include files

---
+++ extension-host/include/sqlexternallibrary.h
@@ -0,0 +1,49 @@
+//*********************************************************************
+//      Copyright (c) Microsoft Corporation.
+//
+// Library management protocol between Exthost and a 3rd party extension
+//
+// @File: sqlexternallibrary.h
+//
+//*********************************************************************
+
+#ifndef __SQLEXTERNALLIBRARY
+#define __SQLEXTERNALLIBRARY
+
+#include "sqlexternallanguage.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif	/* __cplusplus */
+
+// Optional API
+//
+SQLEXTENSION_INTERFACE
+SQLRETURN InstallExternalLibrary(
+	SQLGUID	   SetupSessionId,
+	SQLCHAR	   *LibraryName,
+	SQLINTEGER LibraryNameLength,
+	SQLCHAR	   *LibraryFile,
+	SQLINTEGER LibraryFileLength,
+	SQLCHAR	   *LibraryInstallDirectory,
+	SQLINTEGER LibraryInstallDirectoryLength,
+	SQLCHAR	   **LibraryError,
+	SQLINTEGER *LibraryErrorLength);
+
+// Optional API
+//
+SQLEXTENSION_INTERFACE
+SQLRETURN UninstallExternalLibrary(
+	SQLGUID	   SetupSessionId,
+	SQLCHAR	   *LibraryName,
+	SQLINTEGER LibraryNameLength,
+	SQLCHAR	   *LibraryInstallDirectory,
+	SQLINTEGER LibraryInstallDirectoryLength,
+	SQLCHAR	   **LibraryError,
+	SQLINTEGER *LibraryErrorLength);
+
+#ifdef __cplusplus
+}	/* End of extern "C" { */
+#endif	/* __cplusplus */
+
+#endif	/* __SQLEXTERNALLIBRARY */
