b119ed1178787e03b72020edde03e4fc4313dd56
SicongLiuSimon <sicong.liu.simon@outlook.com>
2021-08-10 15:41:59 -0400

    General Updates

---
+++ restore-packages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install unixodbc-dev for development headers
+#
+apt-get -q -y install unixodbc-dev
+
+apt-get update -y
+apt-get install build-essential software-properties-common -y
+add-apt-repository ppa:ubuntu-toolchain-r/test -y 
+apt-get update -y
+apt-get install gcc-7 g++-7 -y 
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 70 --slave /usr/bin/g++ g++ /usr/bin/g++-7
+
+exit $?
+
