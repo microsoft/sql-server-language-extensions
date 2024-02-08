7661ff6e31b979a6990cecb9b4543cf639c0698b
SicongLiu2000 <sicongliu@microsoft.com>
2024-02-08 13:14:39 -0800

    [Csharp Extension] Removal of PDB Files from C# Extension Release Build (#47)
    * C# pdb
    
    * Resolve Comments

--- language-extensions/dotnet-core-CSharp/build/windows/create-dotnet-core-CSharp-extension-zip.cmd
+++ language-extensions/dotnet-core-CSharp/build/windows/create-dotnet-core-CSharp-extension-zip.cmd
@@ -21,21 +21,25 @@ SET BUILD_OUTPUT=%DOTNET_EXTENSION_WORKING_DIR%\%BUILD_CONFIGURATION%
 MKDIR %BUILD_OUTPUT%\packages
 
 REM Delete the ref folder so that the zip can be loaded by the SPEES
+REM
 RD /S /Q %BUILD_OUTPUT%\ref
 POPD
+
+REM Define files to compress, conditionally including .pdb files if BUILD_CONFIGURATION is "debug"
+REM
+SET FILES_TO_COMPRESS=%BUILD_OUTPUT%\*.dll, %BUILD_OUTPUT%\Microsoft.SqlServer.CSharpExtension.runtimeconfig.json, %BUILD_OUTPUT%\Microsoft.SqlServer.CSharpExtension.deps.json
+IF /I "%BUILD_CONFIGURATION%"=="debug" (SET FILES_TO_COMPRESS=%FILES_TO_COMPRESS%, %BUILD_OUTPUT%\*.pdb)
 
 REM Package the signed binaries.
 REM
 powershell -NoProfile -ExecutionPolicy Unrestricted ^
- -Command "Compress-Archive -Force -Path %BUILD_OUTPUT%\*.dll, %BUILD_OUTPUT%\*.pdb, `" ^
- "%BUILD_OUTPUT%\Microsoft.SqlServer.CSharpExtension.runtimeconfig.json, `" ^
- "%BUILD_OUTPUT%\Microsoft.SqlServer.CSharpExtension.deps.json `" ^
+ -Command "Compress-Archive -Force -Path %FILES_TO_COMPRESS%" ^
  "-DestinationPath %BUILD_OUTPUT%\packages\dotnet-core-CSharp-lang-extension.zip"
 CALL :CHECKERROR %ERRORLEVEL% "Error: Failed to create zip for dotnet-core-CSharp-extension for configuration=%BUILD_CONFIGURATION%" || EXIT /b %ERRORLEVEL%
 
 ECHO "Success: Compressed dotnet-core-CSharp-extension for %BUILD_CONFIGURATION% configuration."
 
-REM REM Advance arg passed to create-dotnet-core-CSharp-extension-zip.cmd
+REM Advance arg passed to create-dotnet-core-CSharp-extension-zip.cmd
 REM
 SHIFT
 REM Continue building using more configs until argv has been exhausted
@@ -45,8 +49,8 @@ IF NOT "%~1"=="" GOTO LOOP
 EXIT /b %ERRORLEVEL%
 
 :CHECKERROR
-	IF %1 NEQ 0 (
-		ECHO %2
-		EXIT /b %1
-	)
-	EXIT /b 0
+    IF %1 NEQ 0 (
+        ECHO %2
+        EXIT /b %1
+    )
+    EXIT /b 0
