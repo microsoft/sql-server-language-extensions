56de03130bb993b6ea4fa420f52ad41c8cf0edce
SicongLiu2000 <sicongliu@microsoft.com>
2024-02-08 12:55:46 -0800

    Resolve Comments

--- language-extensions/dotnet-core-CSharp/build/windows/create-dotnet-core-CSharp-extension-zip.cmd
+++ language-extensions/dotnet-core-CSharp/build/windows/create-dotnet-core-CSharp-extension-zip.cmd
@@ -21,16 +21,20 @@ SET BUILD_OUTPUT=%DOTNET_EXTENSION_WORKING_DIR%\%BUILD_CONFIGURATION%
 MKDIR %BUILD_OUTPUT%\packages
 
 REM Delete the ref folder so that the zip can be loaded by the SPEES
+REM
 RD /S /Q %BUILD_OUTPUT%\ref
 POPD
 
 REM Define files to compress, conditionally including .pdb files if BUILD_CONFIGURATION is "debug"
+REM
 SET FILES_TO_COMPRESS=%BUILD_OUTPUT%\*.dll, %BUILD_OUTPUT%\Microsoft.SqlServer.CSharpExtension.runtimeconfig.json, %BUILD_OUTPUT%\Microsoft.SqlServer.CSharpExtension.deps.json
 IF /I "%BUILD_CONFIGURATION%"=="debug" (SET FILES_TO_COMPRESS=%FILES_TO_COMPRESS%, %BUILD_OUTPUT%\*.pdb)
 
 REM Package the signed binaries.
 REM
-powershell -NoProfile -ExecutionPolicy Unrestricted -Command "Compress-Archive -Force -Path %FILES_TO_COMPRESS% -DestinationPath %BUILD_OUTPUT%\packages\dotnet-core-CSharp-lang-extension.zip"
+powershell -NoProfile -ExecutionPolicy Unrestricted ^
+ -Command "Compress-Archive -Force -Path %FILES_TO_COMPRESS%" ^
+ "-DestinationPath %BUILD_OUTPUT%\packages\dotnet-core-CSharp-lang-extension.zip"
 CALL :CHECKERROR %ERRORLEVEL% "Error: Failed to create zip for dotnet-core-CSharp-extension for configuration=%BUILD_CONFIGURATION%" || EXIT /b %ERRORLEVEL%
 
 ECHO "Success: Compressed dotnet-core-CSharp-extension for %BUILD_CONFIGURATION% configuration."
