4c67f335b55d843299455edf41fbdd61da91016e
Folashade Daniel <fdaniel@microsoft.com>
2025-06-09 16:22:33 -0700

    update boost paths

--- language-extensions/python/build/linux/restore-packages.sh
+++ language-extensions/python/build/linux/restore-packages.sh
@@ -17,7 +17,7 @@ PYTHON_VERSION=3.12
 NUMPY_VERSION=1.26.0
 PANDAS_VERSION=1.4.2
 
-apt-get install -y python-dev python3-dev python${PYTHON_VERSION}-dev libboost-all-dev python${PYTHON_VERSION}-distutils
+apt-get install -y python-dev python3-dev libboost-all-dev
 curl -sS https://bootstrap.pypa.io/get-pip.py | ${PYTHONHOME}/bin/python${PYTHON_VERSION}
 
 # Find PYTHONHOME from user, or set to default for tests.
