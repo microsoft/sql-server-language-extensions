1806b6ee16a327eb77dec35391e8670d9b335955
Sicong Liu <sicongliu@microsoft.com>
2026-07-22 13:33:54 -0700

    Enforce one GCC 11 toolchain for Linux extensions

--- restore-packages.sh
+++ restore-packages.sh
@@ -6,13 +6,16 @@ apt-get -q -y install unixodbc-dev
 
 apt-get update -y
 apt-get install build-essential software-properties-common -y
-# Use GCC 11 (native on the Ubuntu 22.04 build image) so the extension links against
-# GLIBCXX_3.4.29 / glibc 2.35 and stays loadable on RHEL9 (glibc 2.34). GCC 13 emitted
-# GLIBCXX_3.4.32 and glibc 2.38 symbol references (_dl_find_object, arc4random,
-# __isoc23_*) that RHEL9 cannot satisfy, so libPythonExtension.so failed to load there.
+# Use one GCC 11 toolchain for every Linux language extension. DSLE is responsible
+# for supplying compatible runtime headers and OS baselines for each language.
 apt-get install gcc-11 g++-11 -y
 apt-get install cmake -y
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 70 --slave /usr/bin/g++ g++ /usr/bin/g++-11
+
+test "$(gcc -dumpfullversion -dumpversion | cut -d. -f1)" = "11"
+test "$(g++ -dumpfullversion -dumpversion | cut -d. -f1)" = "11"
+gcc --version | head -1
+g++ --version | head -1
 
 exit $?
 
